1825cea0630aa027a9f6b3b349cea6832211be0d
Azheem Rahman <azheem.rahman@gmail.com>
2024-11-22 14:51:32 +0800

    add print statement prompt

---
+++ src/components/PrintPrompt.ts
@@ -0,0 +1,12 @@
+import Input from "./Input";
+
+const PrintPrompt = async (): Promise<string> => {
+  const userInput = await Input(
+    `\nPlease enter account and month to generate the statement <Account> <Year><Month>\n(or enter blank to go back to main menu):\n`
+  );
+  console.log(`You entered: ${userInput}\n`);
+
+  return userInput;
+};
+
+export default PrintPrompt;
