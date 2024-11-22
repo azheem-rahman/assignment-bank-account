11dacb6071bd88794aa7d59e38b9d8c25eabd9f2
Azheem Rahman <azheem.rahman@gmail.com>
2024-11-22 14:51:20 +0800

    add interest rate prompt

---
+++ src/components/InterestPrompt.ts
@@ -0,0 +1,13 @@
+import Input from "./Input";
+
+// shows "Please enter interest rules details in <Date>..."
+const InterestPrompt = async (): Promise<string> => {
+  const userInput = await Input(
+    `\nPlease enter interest rules details in <Date> <RuleId> <Rate in %> format\n(or enter blank to go back to main menu):\n`
+  );
+  console.log(`You entered: ${userInput}\n`);
+
+  return userInput;
+};
+
+export default InterestPrompt;
