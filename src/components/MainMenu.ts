83366db48608552862c0fb7d25336e21da23d3fe
Azheem Rahman <azheem.rahman@gmail.com>
2024-11-23 16:20:20 +0800

    rename launch prompt component to main menu

--- src/components/MainMenu.ts
+++ src/components/MainMenu.ts
@@ -1,7 +1,7 @@
-import Input from "./Input";
+import Input from "./common/Input";
 
-// LaunchPrompt shows "Welcome to AwesomeGIC Bank... and returns user's input"
-const LaunchPrompt = async (): Promise<string> => {
+// MainMenu shows "Welcome to AwesomeGIC Bank... and returns user's input"
+const MainMenu = async (): Promise<string> => {
   const userInput = await Input(
     "\nWelcome to AwesomeGIC Bank! What would you like to do?\n[T] Input transactions\n[I] Define interest rules\n[P] Print statement\n[Q] Quit\n"
   );
@@ -10,4 +10,4 @@ const LaunchPrompt = async (): Promise<string> => {
   return userInput;
 };
 
-export default LaunchPrompt;
+export default MainMenu;
